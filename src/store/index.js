5b26f09de2f5625586327361f095099c85356236
yerlanyr <yerlanyr@gmail.com>
2019-01-30 17:58:35 +0600

    Install redux

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+
+const robots = (state = [], action) => {
+    switch(action.type){
+        //case '': 
+        default: return state;
+    }
+}
+
+export default createStore(combineReducers({
+
+}));
